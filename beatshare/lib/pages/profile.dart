6eb851b87e443e29aa25d108434984ccec8f7f85
Dean Mulrooney <mulrooney.dean@gmail.com>
2020-05-07 10:03:33 +0100

    Edit profile screen and functionality

--- beatshare/lib/pages/profile.dart
+++ beatshare/lib/pages/profile.dart
@@ -1,9 +1,10 @@
-import 'package:beatshare/models/user.dart';
-import 'package:beatshare/pages/timeline.dart';
-import 'package:beatshare/widgets/header.dart';
-import 'package:beatshare/widgets/progress.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:beatshare/models/user.dart';
+import 'package:beatshare/pages/edit_profile.dart';
+import 'package:beatshare/pages/home.dart';
+import 'package:beatshare/widgets/header.dart';
+import 'package:beatshare/widgets/progress.dart';
 
 class Profile extends StatefulWidget {
   final String profileId;
@@ -15,6 +16,8 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final String currentUserId = currentUser?.id;
+
   Column buildCountColumn(String label, int count) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -39,10 +42,48 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-  buildProfileButton() {
-    return Text("profile button");
+  editProfile() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => EditProfile(currentUserId: currentUserId)));
   }
 
+  Container buildButton({String text, Function function}) {
+    return Container(
+      padding: EdgeInsets.only(top: 2.0),
+      child: FlatButton(
+        onPressed: function,
+        child: Container(
+          width: 250.0,
+          height: 27.0,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(
+              color: Colors.blue,
+            ),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildProfileButton() {
+    // viewing your own profile - should show edit profile button
+    bool isProfileOwner = currentUserId == widget.profileId;
+    if (isProfileOwner) {
+      return buildButton(text: "Edit Profile", function: editProfile);
+    }
+  }
 // header for profile
   buildProfileHeader() {
     return FutureBuilder(
@@ -51,8 +92,7 @@ class _ProfileState extends State<Profile> {
         if (!snapshot.hasData) {
           return circularProgress();
         }
-
-        // widget for profil pic
+                // widget for profile pic
         User user = User.fromDocument(snapshot.data);
         return Padding(
           padding: EdgeInsets.all(16.0),
